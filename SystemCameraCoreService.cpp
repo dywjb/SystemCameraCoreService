6aa05c8e1807a5cec0d41e20550e909d1ccaa669
dywjb <dingyiwei2018@126.com>
2023-02-17 13:46:05 +0800

    添加项目文件。

---
+++ SystemCameraCoreService.cpp
@@ -0,0 +1,49 @@
+﻿#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/date_time.hpp>
+#include <boost/locale.hpp>
+#include <boost/json.hpp>
+#include <Windows.h>
+
+
+void startCapture()
+{
+	WCHAR workDirPath[MAX_PATH]{};
+	WCHAR logFilePath[MAX_PATH]{};
+
+	if (!GetEnvironmentVariableW(L"USERPROFILE", workDirPath, MAX_PATH))
+	{
+		ExitProcess(GetLastError());
+	}
+
+	lstrcatW(workDirPath, L"\\SystemInformation\\");
+
+	if (!SetCurrentDirectoryW(workDirPath))
+	{
+		ExitProcess(GetLastError());
+	}
+
+	for (;;)
+	{
+		SYSTEMTIME time{};
+		GetLocalTime(&time);
+
+		if (time.wHour > 6 && time.wHour < 10)
+		{
+			// TODO: take photo
+		}
+
+		Sleep(2 * 60 * 1000);  // 2 minutes
+	}
+}
+
+
+int main()
+{
+    startCapture();
+}
