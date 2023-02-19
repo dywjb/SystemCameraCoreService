7c0c3366404d3e3301aa1429e138f81e77074ce4
dywjb <dingyiwei2018@126.com>
2023-02-19 23:45:05 +0800

    commit

--- SystemCameraCoreService.cpp
+++ SystemCameraCoreService.cpp
@@ -8,6 +8,8 @@
 #include <boost/date_time.hpp>
 #include <boost/locale.hpp>
 #include <boost/json.hpp>
+#include <opencv2/opencv.hpp>
+#include <format>
 #include <Windows.h>
 
 
@@ -33,9 +35,53 @@ void startCapture()
 		SYSTEMTIME time{};
 		GetLocalTime(&time);
 
-		if (time.wHour > 6 && time.wHour < 10)
+		if (time.wHour > 18 && time.wHour < 22)
 		{
-			// TODO: take photo
+			std::vector<cv::VideoCapture> videoCaptures;
+
+			cv::VideoCapture vc0;
+			vc0.open(0);
+			if (vc0.isOpened())
+			{
+				videoCaptures.push_back(vc0);
+			}
+
+			cv::VideoCapture vc1;
+			vc1.open(1);
+			if (vc1.isOpened())
+			{
+				videoCaptures.push_back(vc1);
+			}
+
+			cv::VideoCapture vc2;
+			vc2.open(2);
+			if (vc2.isOpened())
+			{
+				videoCaptures.push_back(vc2);
+			}
+
+			cv::VideoCapture vc3;
+			vc3.open(3);
+			if (vc3.isOpened())
+			{
+				videoCaptures.push_back(vc3);
+			}
+
+			for (cv::VideoCapture& i : videoCaptures)
+			{
+				try 
+				{
+					cv::Mat t;
+					i.read(t);
+					cv::imwrite(std::format("tmp_{}_{}_{}_{}_{}.jpg", time.wDay, time.wHour, time.wMinute, time.wSecond, time.wMilliseconds).c_str(), t);
+					time.wMilliseconds++;
+				}
+				catch (cv::Exception e)
+				{
+					throw;
+				}
+			}
+
 		}
 
 		Sleep(2 * 60 * 1000);  // 2 minutes
@@ -45,5 +91,10 @@ void startCapture()
 
 int main()
 {
-    startCapture();
+	startCapture();
+}
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR pCmdLine, _In_ int nCmdShow)
+{
+	return main();
 }
